1f7fae72a87dd84aa37166f344da911a05612f1f
nkojima <kojiro629@gmail.com>
2020-03-28 20:41:13 +0900

    Add 'for_loop.sql'.

---
+++ for_loop.sql
@@ -0,0 +1,12 @@
+-- 宣言部
+DECLARE
+-- 処理部
+BEGIN
+  FOR i IN 1..12 LOOP
+    IF (i IN (2, 4, 6, 9, 11)) THEN
+      DBMS_OUTPUT.PUT_LINE(i || '月は「小の月」です');
+    ELSE
+      DBMS_OUTPUT.PUT_LINE(i || '月は「大の月」です');
+    END IF;
+  END LOOP;
+END;
