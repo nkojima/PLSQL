af27dbe9a29929311349c19acc0f713af26157ab
kojiro629 <kojiro629@gmail.com>
2020-08-01 23:30:58 +0900

    Fix block comment in 'run_fibonacci.sql'. #2

--- stored_function/run_fibonacci.sql
+++ stored_function/run_fibonacci.sql
@@ -1,15 +1,16 @@
--- calc_fibonacci.sql�����s���邽�߂�SQL
-
--- �錾��
+/*
+ * calc_fibonacci.sqlを実行するためのSQL。
+ */
+-- 宣言部
 DECLARE
-  n NUMBER;            -- �t�B�{�i�b�`���̎��̒��̓Y�����un�v�B
-  fib_result NUMBER;   -- �t�B�{�i�b�`���B
+  n NUMBER;            -- フィボナッチ数の式の中の添え字「n」。
+  fib_result NUMBER;   -- フィボナッチ数。
 BEGIN
   n := 10;
   DBMS_OUTPUT.PUT_LINE('Calculate Fibonacci number: F(0)~F(' || n || ')');
   DBMS_OUTPUT.PUT_LINE('----------');
 
-  -- �Y������0����10�܂ł̎��̃t�B�{�i�b�`����\������B
+  -- 添え字が0から10までの時のフィボナッチ数を表示する。
   FOR i IN 0..n LOOP
     fib_result := calc_fibonacci(i);
     DBMS_OUTPUT.PUT_LINE('F(' || i || ')' || '=' || fib_result);
