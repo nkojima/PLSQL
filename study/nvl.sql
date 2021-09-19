4491c760f841bc7a35399fd8974e641aea6eb727
kojiro629 <kojiro629@gmail.com>
2021-09-19 22:45:30 +0900

    Update nvl.sql #19

--- study/nvl.sql
+++ study/nvl.sql
@@ -1,20 +1,28 @@
--- NVL関数を使わないSQL。
+-- 営業部門の全員を抽出しているだけのSQL。
+-- NVL関数は使っていない。
 SELECT
-  T1.DNAME AS 部署名,
-  T2.ENAME AS 社員名
+  T1.EMPNO AS 社員番号,
+  T1.ENAME AS 社員名,
+  T1.JOB AS 職種,
+  T1.COMM AS 歩合給
 FROM
-  SCOTT.DEPT T1
-  LEFT OUTER JOIN SCOTT.EMP T2 ON T1.DEPTNO=T2.DEPTNO
+  SCOTT.EMP T1,
+  SCOTT.DEPT T2
 WHERE
-  T1.DNAME IN ('ACCOUNTING', 'OPERATIONS');
+  T1.DEPTNO=T2.DEPTNO
+  AND T2.DNAME='SALES';
 
 
--- NVL関数を使って、部署に紐づく社員が居ない時に「---」を出力するSQL。
+-- 営業部門の全員を抽出しているだけのSQL。
+-- NVL関数を使って、歩合給がnullの場合に「0」を返している。
 SELECT
-  T1.DNAME AS 部署名,
-  NVL(T2.ENAME, '---') AS 社員名
+  T1.EMPNO AS 社員番号,
+  T1.ENAME AS 社員名,
+  T1.JOB AS 職種,
+  NVL(T1.COMM, 0) AS 歩合給
 FROM
-  SCOTT.DEPT T1
-  LEFT OUTER JOIN SCOTT.EMP T2 ON T1.DEPTNO=T2.DEPTNO
+  SCOTT.EMP T1,
+  SCOTT.DEPT T2
 WHERE
-  T1.DNAME IN ('ACCOUNTING', 'OPERATIONS');
+  T1.DEPTNO=T2.DEPTNO
+  AND T2.DNAME='SALES';
